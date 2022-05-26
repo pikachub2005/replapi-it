e22414e6143bd6fce9b7b9b96d199454a92546f1
PikachuB2005 <74213212+pikachub2005@users.noreply.github.com>
2022-05-26 18:38:27 +0000

    Added todo.md

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const Client = require(__dirname + "/src/index.js");
 const client = new Client(process.env.PikachuB2005);
 
-client.on("ready", async () => {
-	let repls = await client.user.repls.fetch();
-	let repl = repls.find(r => r.title == "pika-replitclient");
-	let files = repl.files;
-	await repl.connect();
-	console.log(await files.read("src/index.js"));
-	await repl.disconnect();
+client.on("ready", async () => {3
+	console.log(client);
+	// let repls = await client.user.repls.fetch();
+	// let repl = repls.find(r => r.title == "pika-replitclient");
+	// let files = repl.files;
+	// await repl.connect();
+	// console.log(await files.read("src/index.js"));
+	// await repl.disconnect();
 })
