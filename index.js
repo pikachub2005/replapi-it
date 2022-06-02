9dfb434714268f2fbe581a9feb1a450feefd00dd
PikachuB2005 <74213212+pikachub2005@users.noreply.github.com>
2022-06-02 17:04:00 +0000

    Removed index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Client = require("./src/index.js");
-const client = new Client(process.env.PikachuB2005);
-
-client.on("ready", async () => {
-	let user = await client.users.fetch(process.env.REPL_OWNER);
-	let repls = await user.repls.fetch();
-	let repl = repls.first();
-	console.log("Your newest repl:", repl.title);
-})
